525c4d17c6e664aa86de8d203ea8df6496df4eb3
mtmccarthy <mtmccarthy@wpi.edu>
2016-09-13 21:04:38 -0400

    Crossover
    Added crossover, mutate still stubbed

--- src/genetic/Organism.java
+++ src/genetic/Organism.java
@@ -45,19 +45,22 @@ public class Organism {
 	{
 		//Determine which organism has more operators
 		int minAllelles;
+		int maxAllelles;
 		int thisSize = this.getOperators().size();
 		int thatSize = o.getOperators().size();
 		if(thisSize <= thatSize) {
 			minAllelles = thisSize;
+			maxAllelles = thatSize;
 		}
 		else {
 			minAllelles = thatSize;
+			maxAllelles = thisSize;
 		}
 
 		Random ran = new Random();
 		int crossoverPivot = ran.nextInt() % minAllelles;
 		//Crossover contains mutation
-		return this.crossover(this, o, crossoverPivot);
+		return this.crossover(this, o, crossoverPivot, maxAllelles);
 	}
 	
 	private void mutate()
@@ -70,10 +73,33 @@ public class Organism {
 		return 0.0;
 	}
 
-	private LinkedList<Organism> crossover(Organism father, Organism mother, int crossoverPivot) {
+	private LinkedList<Organism> crossover(Organism father, Organism mother, int crossoverPivot, int maxsize) {
 		//contains mutation
 
-		return null;
+		LinkedList<String> childAOps = new LinkedList<String>();
+		LinkedList<String> childBOps = new LinkedList<String>();
+
+		//crossover
+		for(int i = 0; i < crossoverPivot; i++) {//make sure to come back and check corner cases here
+			childAOps.add(i, father.getOperators().get(i));
+			childBOps.add(i, mother.getOperators().get(i));
+		}
+		for(int j = crossoverPivot; j < maxsize; j++) {
+			childAOps.add(j, mother.getOperators().get(j));
+			childBOps.add(j, father.getOperators().get(j));
+		}
+		Organism childA = new Organism(childAOps);
+		Organism childB = new Organism(childBOps);
+
+		//mutate - small percentage chance of mutating
+		childA.mutate();
+		childB.mutate();
+
+		LinkedList<Organism> children = new LinkedList<Organism>();
+		children.add(childA);
+		children.add(childB);
+
+		return children;
 	}
 	
 	
